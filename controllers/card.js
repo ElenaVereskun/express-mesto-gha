d3243bc81ceab8fd823f630d862945481b0350b0
Elena Vereskun <lena.vereskun@yandex.ru>
2023-07-18 21:12:20 +0300

    instanceof

--- controllers/card.js
+++ controllers/card.js
@@ -1,71 +1,75 @@
-const cards = require('../models/card');
+const { default: mongoose, MongooseError } = require('mongoose');
+const Card = require('../models/card');
 
-const ERROR_CODE = 400;
+const ERROR_BAD_REQUEST = 400;
+const ERROR_NOT_FOUND = 404;
+const ERROR_INTERNAL_SERVER = 500;
+const STATUS_OK = 200;
 
 module.exports.getCards = (req, res) => {
-  cards.find({})
-    .then((card) => res.status(200).send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
+  Card.find({})
+    .then((card) => res.status(STATUS_OK).send({ data: card }))
+    .catch(() => res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
-  return cards.create({ name, link, owner })
-    .then((card) => res.status(201).send({ data: card }))
+  return Card.create({ name, link, owner })
+    .then((card) => res.status(STATUS_OK).send({ data: card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные' });
+      if (err instanceof MongooseError.ValidationError) {
+        return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
-      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+      return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
     });
 };
 
 module.exports.deleteCard = (req, res) => {
-  cards.findByIdAndRemove(req.params.cardId)
+  Card.findByIdAndRemove(req.params.cardId)
     .orFail(new Error('NotValidId'))
-    .then((card) => res.status(200).send(card))
+    .then((card) => res.status(STATUS_OK).send(card))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные' });
+      if (err instanceof mongoose.CastError) {
+        return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
       if (err.message === 'NotValidId') {
-        return res.status(404).send({ message: ' Карточка с указанным _id не найдена.' });
+        return res.status(ERROR_NOT_FOUND).send({ message: ' Карточка с указанным _id не найдена.' });
       }
-      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+      return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
     });
 };
 
-module.exports.likeCard = (req, res) => cards.findByIdAndUpdate(
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } },
   { new: true },
 )
   .orFail(new Error('NotValidId'))
-  .then((card) => res.status(201).send(card))
+  .then((card) => res.status(STATUS_OK).send(card))
   .catch((err) => {
-    if (err.name === 'CastError') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные для постановки лайка.' });
+    if (err instanceof mongoose.CastError) {
+      return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки лайка.' });
     }
     if (err.message === 'NotValidId') {
-      return res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+      return res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки.' });
     }
-    return res.status(500).send({ message: 'Ошибка по умолчанию' });
+    return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
   });
 
-module.exports.dislikeCard = (req, res) => cards.findByIdAndUpdate(
+module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } },
   { new: true },
 )
   .orFail(new Error('NotValidId'))
-  .then((card) => res.status(200).send(card))
+  .then((card) => res.status(STATUS_OK).send(card))
   .catch((err) => {
-    if (err.name === 'CastError') {
-      return res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные для снятия лайка.' });
+    if (err instanceof mongoose.CastError) {
+      return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для снятия лайка.' });
     }
     if (err.message === 'NotValidId') {
-      return res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+      return res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки.' });
     }
-    return res.status(500).send({ message: 'Ошибка по умолчанию' });
+    return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
   });
--- controllers/user.js
+++ controllers/user.js
@@ -1,35 +1,41 @@
+const { default: mongoose } = require('mongoose');
 const User = require('../models/user');
+
+const ERROR_BAD_REQUEST = 400;
+const ERROR_NOT_FOUND = 404;
+const ERROR_INTERNAL_SERVER = 500;
+const STATUS_OK = 200;
 
 module.exports.postUser = (req, res) => {
   const { name, about, avatar } = req.body;
   return User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => res.status(STATUS_OK).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные  при создании пользователя' });
+        return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные  при создании пользователя' });
       }
-      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+      return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
     });
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию' }));
+    .then((users) => res.status(STATUS_OK).send(users))
+    .catch(() => res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' }));
 };
 
 module.exports.getUserId = (req, res) => {
   User.findById(req.params.userId)
     .orFail(new Error('NotValidId'))
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(STATUS_OK).send(user))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные' });
+      if (err instanceof mongoose.CastError) {
+        return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
       if (err.message === 'NotValidId') {
-        return res.status(404).send({ message: ' Пользователь по указанному _id не найден' });
+        return res.status(ERROR_NOT_FOUND).send({ message: ' Пользователь по указанному _id не найден' });
       }
-      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+      return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
     });
 };
 
@@ -38,15 +44,15 @@ module.exports.updateProfile = (req, res) => {
   const userId = req.user._id;
   User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
     .orFail(new Error('NotValidId'))
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(STATUS_OK).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: ' Переданы некорректные данные при обновлении профиля' });
+        return res.status(ERROR_BAD_REQUEST).send({ message: ' Переданы некорректные данные при обновлении профиля' });
       }
       if (err.message === 'NotValidId') {
-        return res.status(404).send({ message: ' Пользователь по указанному _id не найден' });
+        return res.status(ERROR_NOT_FOUND).send({ message: ' Пользователь по указанному _id не найден' });
       }
-      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+      return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
     });
 };
 
@@ -55,14 +61,14 @@ module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
     .orFail(new Error('NotValidId'))
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(STATUS_OK).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: ' Переданы некорректные данные при обновлении аватара' });
+        return res.status(ERROR_BAD_REQUEST).send({ message: ' Переданы некорректные данные при обновлении аватара' });
       }
       if (err.message === 'NotValidId') {
-        return res.status(404).send({ message: ' Пользователь с указанным _id не найден.' });
+        return res.status(ERROR_NOT_FOUND).send({ message: ' Пользователь с указанным _id не найден.' });
       }
-      return res.status(500).send({ message: 'Ошибка по умолчанию' });
+      return res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
     });
 };
